946e367bb90fe87373d11d5aff583c2728d6c927
Nurullo <94496332+NurullohMahmud@users.noreply.github.com>
2023-11-17 13:20:38 -0600

    debugged

--- main/views.py
+++ main/views.py
@@ -6,42 +6,37 @@ from . import serializers as s
 from . import models as m 
 
 
-class ProjectRetrieveAPIView(APIView):
-    def get(self, request):
-        projects = m.Project.objects.all()
-        serialized = s.ProjectSerializer(projects, many=True)
-        return Response(status=200, data=serialized.data)
-
 
 class ProjectCreateAPIView(APIView):
     permission_classes = (IsAuthenticated, )
 
     def post(self, request):
         serialized = s.ProjectSerializer(data=request.data)
-        serialized.is_valid(raise_exception=True)
-        return Response(status=200, data=serialized.data)
+        if serialized.is_valid():
+            serialized.save()
+            return Response(status=200, data=serialized.data)
+        return Response(serialized.errors, status=400)
 
 
 class ProjectUpdateDeleteAPIView(APIView):
     def put(self, request, pk):
         instance = get_object_or_404(m.Project, pk=pk)
-        if instance:
-            serialized = s.ProjectSerializer(data=instance)
-            serialized.is_valid(raise_exception=True)
+        serialized = s.ProjectSerializer(instance, data=request.data)
+        if serialized.is_valid():
             serialized.save()
             return Response(status=200, data=serialized.data)
-        return Response(status=400)
+        return Response(serialized.errors, status=400)
 
     def patch(self, request, pk):
         instance = get_object_or_404(m.Project, pk=pk)
-        serialized = s.ProjectSerializer(data=instance, partial=True)
-        serialized.is_valid(raise_exception=True)
-        serialized.save()
-        return Response(status=200, data=serialized.data)
+        serialized = s.ProjectSerializer(instance, data=request.data, partial=True)
+        if serialized.is_valid():
+            serialized.save()
+            return Response(status=200, data=serialized.data)
+        return Response(serialized.errors, status=400)
     
     def delete(self, request, pk):
         instance = get_object_or_404(m.Project, pk=pk)
-        if instance:
-            instance.delete()
-            return Response(status=200, data={"Message": "Deleted successfully"})
-        
+        instance.delete()
+        return Response(status=200, data={"Message": "Deleted successfully"})
+    
